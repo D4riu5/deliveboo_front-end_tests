c69536b7524da26fc7995bb43399ff9a4817003a
D4riu5 <R3iji94@gmail.com>
2023-04-21 14:27:06 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+// components
+import AppHeader from "./components/AppHeader.vue";
+import Component1 from "./components/Component1.vue";
+import { store } from "./store.js";
+
+export default {
+    name: "App",
+    components: {
+        Component1,
+        AppHeader,
+    },
+    data() {
+        return {
+            store,
+        };
+    },
+    methods: {},
+};
+</script>
+
+<template>
+    <AppHeader />
+    <Main class="p-5 container">
+        <router-view></router-view>
+    </Main>
+    <Component1 />
+</template>
+
+<style lang="scss">
+@use "./styles/main";
+</style>
